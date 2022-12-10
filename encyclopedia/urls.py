a67270918580b2aa6ef9d861ec6738775c297c5c
Aditya Upadhyay <adityau930@gmail.com>
2022-12-11 00:20:28 +0530

    Home redirect, fixed Random Page

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("<title>", views.display_entry, name="entry"),
     path("new/", views.new_entry, name="new"),
+    path("<title>/edit", views.new_entry, name="edit"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,22 +1,40 @@
 from django.shortcuts import render
 
 from . import util
-import markdown2, random
+import markdown2
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
-def new_entry(request):
+def new_entry(request, title=""):
     if request.method =="GET":
+        if title:
+            content=util.get_entry(title)
+        else:
+            content=""
         return render(request, "encyclopedia/new_entry.html", {
-            "entries": util.list_entries()
+            "entries": util.list_entries(),
+            "content": content,
         })
-    else:
-        util.save_entry(request.POST["title"], request.POST["content"])
-        return index(request)
 
+    else:
+        title, content= request.POST["title"], request.POST["content"]
+        if title not in util.list_entries():
+            util.save_entry(title, content)
+            return display_entry(request, title)
+        else:
+            return render(request, 'error_page.html', {
+                'error_message':"There is already an entry with the same name."
+            } )
+
+def edit_entry(request, title):
+    content=util.get_entry(title)
+    return render(request, "encyclopedia/new_entry.html", {
+        "entries": util.list_entries(),
+        "content": content
+    } )
 
 def display_entry(request, title):
     content=util.get_entry(title)
